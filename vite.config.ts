d9f8386f9658d61e07157db0985348f0d8a435d5
achotai2 <achotai2@gmail.com>
2026-02-11 20:37:20 +0800

    Changed manifest and vite.config to handle git pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ export default defineConfig({
     port: 5173,
     open: false,
   },
+
+  if (command === 'serve') {
+    // DEV MODE (npm run dev)
+    config.base = '/'
+  } else {
+    // BUILD MODE (npm run build)
+    config.base = '/tallies-pwa/'
+  }
+
+  return config
 });
